f8d61f815d3e53446e854a04ef11f1d8a6f4679a
satbir544 <69642217+satbir544@users.noreply.github.com>
2023-12-14 01:07:47 -0500

    added a sample account card and an 'add account' button

--- src/accounts.js
+++ src/accounts.js
@@ -1,3 +1,4 @@
+// src/Accounts.js
 import React from 'react';
 import './accounts.css';
 import SideNav from './sideNav';
@@ -5,9 +6,21 @@ import SideNav from './sideNav';
 function Accounts() {
   return (
     <div className='accounts'>
-      <SideNav/>
+      <SideNav />
       <div className='content'>
         <h2>ACCOUNTS</h2>
+        
+        {/* Sample Card */}
+        <div className='card'>
+          <h3>Savings</h3>
+          <p>Account Number: 1234</p>
+          <p>Balance: $1,000.00</p>
+        </div>
+
+        {/* Add Account Button */}
+        <div className="add-account-button">
+          <button>Add Account</button>
+        </div>
       </div>
     </div>
   );
